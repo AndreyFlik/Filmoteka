7b590755fb846d7e7b103fac73750527a94bbe89
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-11-27 21:22:22 +0200

    final

--- src/js/header.js
+++ src/js/header.js
@@ -1,61 +1,30 @@
 import seachMovies from './services/apiSeach';
-import cardTpl from '../templates/card.hbs';
 import refs from './services/refs.js';
-// refs.trendMovies;
-const inputMovies = document.querySelector('#header__form');
-const searchProblemAlarm = document.querySelector('.form__text');
+import renderMovies from './services/markupMovies';
+import getMovies from './services/getMovies.js';
 
-inputMovies.addEventListener('input', getInputMovies);
+const ERROR_NOT_FOUND = 'Search result not successful. Enter the correct movie name.';
+
+refs.inputMovies.addEventListener('input', getInputMovies);
 
 function getInputMovies(event) {
-  //   console.log(event.target.value);
+  console.log(event.target.value.length);
+  if (!event.target.value.length) {
+    return getMovies();
+  }
   return seachMovies(event.target.value)
     .then(respons => {
-      //   console.log(respons);
       if (respons.total_results === 0) {
-        return searchProblemAlarm.classList.remove('visually-hidden', 'is-hidden');
+        getMovies();
+        refs.searchProblemAlarm.classList.remove('visually-hidden', 'is-hidden');
+        return Promise.reject(new Error(ERROR_NOT_FOUND));
       } else {
-        searchProblemAlarm.classList.add('visually-hidden', 'is-hidden');
-        return respons.results;
+        refs.searchProblemAlarm.classList.add('visually-hidden', 'is-hidden');
+        return respons;
       }
     })
-    .then(results =>
-      results.map(
-        // movie => console.log(movie.poster_path),
-        movie => ({
-          title: movie.title,
-          image: movie.poster_path,
-          genre: movie.genre_ids,
-          year: movie.release_date,
-          vote: movie.vote_average,
-        }),
-        (refs.trendMovies.innerHTML = results.map(item => cardTpl(item)).join('')),
-      ),
-    )
-    .catch(error => console.log(error));
+    .then(data => {
+      renderMovies(data.results);
+    })
+    .catch(error => console.log(error.message));
 }
-// console.log(movie),
-// ({});
-// const data = [
-//   {
-//     title: 'Avengers: Infinity War',
-//     image: 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg',
-//     genre: 'Horror, Comedy',
-//     year: '2020',
-//     vote: '9.6',
-//   },
-//   {
-//     title: 'Deadpool 2',
-//     image: 'https://cdn.pixabay.com/photo/2021/10/27/19/09/cat-6748193_960_720.jpg',
-//     genre: 'Horror, Drama, Musical',
-//     year: '2018',
-//     vote: '8.3',
-//   },
-//   {
-//     title: "To All the Boys I've Loved Before",
-//     image: 'https://cdn.pixabay.com/photo/2017/07/25/01/22/cat-2536662_960_720.jpg',
-//     genre: 'History, Drama',
-//     year: '1986',
-//     vote: '7.2',
-//   },
-// ];
--- src/js/services/refs.js
+++ src/js/services/refs.js
@@ -1,5 +1,7 @@
 const refs = {
   trendMovies: document.querySelector('.gallery-list'),
+  inputMovies: document.querySelector('#header__form'),
+  searchProblemAlarm: document.querySelector('.form__text'),
 };
 
 export default refs;
