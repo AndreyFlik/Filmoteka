9539063ca6d9f7a6c42b390b73a5596082538e07
Yuriy <ukrhero@gmail.com>
2021-11-27 15:22:13 +0200

    Fix apiSearch

--- src/js/services/apiSeach.js
+++ src/js/services/apiSeach.js
@@ -1,13 +1,14 @@
 const BASE_SEACH_URL = 'https://api.themoviedb.org/3/search/movie';
 const API = '1db5479ca98fac1ae129bcbe15cc6182';
 
+let page = 1;
 const seachParams = new URLSearchParams({
   api_key: API,
   page: page,
 });
 
 const seachMovies = query => {
-  return fetch(`${BASE_SEACH_URL}?${seachParams}&page=${page}&query=${query}`).then(res => {
+  return fetch(`${BASE_SEACH_URL}?${seachParams}&query=${query}`).then(res => {
     if (res.ok) return res.json();
     return Promise.reject(new Error('Something went wrong'));
   });
