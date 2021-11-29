329fe8a33e2ecc3226d007b20b7c6122f579b3db
Valerian Rykov <Valerra2@gmail.com>
2021-11-29 10:13:00 +0200

    myLibraryMarkup

--- src/js/services/refs.js
+++ src/js/services/refs.js
@@ -2,6 +2,8 @@ const refs = {
   trendMovies: document.querySelector('.gallery-list'),
   inputMovies: document.querySelector('#header__form'),
   searchProblemAlarm: document.querySelector('.form__text'),
+  watchedBtn: document.querySelector('.header-button-watched'),
+  queueBtn: document.querySelector('.header-button-queue'),
 };
 
 export default refs;
