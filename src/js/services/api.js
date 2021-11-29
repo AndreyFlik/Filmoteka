818ce82e773131eb94fed7457abf6f3ebed47af9
Oleksandr Myrvoda <mirvoda2@gmail.com>
2021-11-29 17:57:59 +0200

    api refactoring2

--- src/js/services/api.js
+++ src/js/services/api.js
@@ -1,14 +1,15 @@
 import 'js-loading-overlay';
 import renderMovies from './markupMovies';
 
-const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/week';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+
 const API = '1db5479ca98fac1ae129bcbe15cc6182';
 const getMovies = (page = 1) => {
   const queryParams = new URLSearchParams({
     api_key: API,
     page: page,
   });
-  return fetch(`${BASE_URL}?${queryParams}`)
+  return fetch(`${BASE_URL}trending/movie/week?${queryParams}`)
     .then(res => {
       if (res.ok) return res.json();
       return Promise.reject(new Error('Something went wrong'));
@@ -23,9 +24,7 @@ const getMovies = (page = 1) => {
 const handleError = err => {
   console.log(err);
 };
-// =================================================
-
-const BASE_SEACH_URL = 'https://api.themoviedb.org/3/search/movie';
+// // =================================================
 
 const configs = {
   overlayBackgroundColor: '#666666',
@@ -47,13 +46,15 @@ const seachMovies = async (query, page = 1) => {
     api_key: API,
     page: page,
   });
-  const response = await fetch(`${BASE_SEACH_URL}?${seachParams}&query=${query}`).then(res => {
-    if (res.ok) return res.json();
-    return Promise.reject(new Error('Something went wrong'));
-  });
+  const response = await fetch(`${BASE_URL}search/movie?${seachParams}&query=${query}`).then(
+    res => {
+      if (res.ok) return res.json();
+      return Promise.reject(new Error('Something went wrong'));
+    },
+  );
   JsLoadingOverlay.hide();
   return response;
 };
-// =================================================
 
+// ==============================
 export { getMovies, seachMovies };
