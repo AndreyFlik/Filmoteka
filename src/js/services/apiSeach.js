6a8bb7e27b85673f59016b4c0dcbe4f5dac0f345
Oleksandr Myrvoda <mirvoda2@gmail.com>
2021-11-29 14:39:34 +0200

    pagination and speener

--- src/js/services/apiSeach.js
+++ src/js/services/apiSeach.js
@@ -3,14 +3,6 @@ import 'js-loading-overlay';
 const BASE_SEACH_URL = 'https://api.themoviedb.org/3/search/movie';
 const API = '1db5479ca98fac1ae129bcbe15cc6182';
 
-const seachMovies = (query, page = 1) => {
-  const seachParams = new URLSearchParams({
-    api_key: API,
-    page: page,
-  });
-
-  return fetch(`${BASE_SEACH_URL}?${seachParams}&query=${query}`).then(res => {
-    if (res.ok) return res.json();
 const configs = {
   overlayBackgroundColor: '#666666',
   overlayOpacity: '0.1',
@@ -25,23 +17,18 @@ const configs = {
   containerID: null,
 };
 
-let page = 1;
-const seachParams = new URLSearchParams({
-  api_key: API,
-  page: page,
-});
-
-async function seachMovies(query) {
+const seachMovies = async (query, page = 1) => {
   JsLoadingOverlay.show(configs);
+  const seachParams = new URLSearchParams({
+    api_key: API,
+    page: page,
+  });
   const response = await fetch(`${BASE_SEACH_URL}?${seachParams}&query=${query}`).then(res => {
-    if (res.ok) {
-      return res.json();
-    }
-
+    if (res.ok) return res.json();
     return Promise.reject(new Error('Something went wrong'));
   });
   JsLoadingOverlay.hide();
   return response;
-}
+};
 
 export default seachMovies;
